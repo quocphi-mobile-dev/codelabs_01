b4d448100386ebc5f8a98aa1d2e6165291c390fb
quocphineu97 <quocphibk1997@gmail.com>
2019-11-19 10:07:45 +0700

    update 1

---
+++ src/servlet/NewFolder.java
@@ -0,0 +1,13 @@
+package servlet;
+
+public class NewFolder  {
+    public double start;
+    public double finish;
+    public String name;
+
+    public NewFolder(double start, double finish, String name) {
+        this.start = start;
+        this.finish = finish;
+        this.name = name;
+    }
+}
